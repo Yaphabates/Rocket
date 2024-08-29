954caaf1b3f7e5613511611173c2cd25e4ebd241
Yaphabates <Yaphabates@sjtu.edu.cn>
2024-08-29 19:12:44 +0800

    update

--- Data_Augmentation/run.sh
+++ Data_Augmentation/run.sh
@@ -1,3 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python data_aug.py \
---opensource_path Rocket/Data/commonsense_qa.json \
---kshot_data_path Rocket/Data/arc-c_50shot.json \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python data_aug.py \
+--opensource_path ../Data/opensouece-data.json \
+--kshot_data_path arc_boolq_piqa.json \
+--target_num 5000
